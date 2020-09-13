1f6aba84ff18aa5ab80abb705991a3d062683ab6
BothEyes <1993046671@qq.com>
2020-09-13 20:20:27 +0800

    初始化

---
+++ src/views/systemSetting/administratorAuthority/adminList/index.vue
@@ -0,0 +1,217 @@
+<template>
+  <div class="divBox">
+    <el-card class="box-card">
+      <el-form inline size="small" @submit.native.prevent>
+        <el-form-item>
+          <el-select v-model="listPram.roles" placeholder="身份" clearable class="selWidth">
+            <el-option
+              v-for="item in roleList.list"
+              :key="item.id"
+              :label="item.roleName"
+              :value="item.id"
+            />
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-select v-model="listPram.status" placeholder="状态" clearable class="selWidth">
+            <el-option
+              v-for="item in constants.roleListStatus"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            />
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="listPram.realName" placeholder="姓名或者账号" clearable class="selWidth"/>
+        </el-form-item>
+        <el-form-item>
+          <el-button size="mini" type="primary" @click="handleSearch">查询</el-button>
+        </el-form-item>
+      </el-form>
+      <el-form inline @submit.native.prevent>
+        <el-form-item>
+          <el-button size="mini" type="primary" @click="handlerOpenEdit(0)">添加管理员</el-button>
+        </el-form-item>
+      </el-form>
+      <el-table :data="listData.list" size="mini">
+        <el-table-column
+          prop="id"
+          label="ID"
+          width="50"
+        />
+        <el-table-column label="姓名" prop="realName" min-width="120"/>
+        <el-table-column label="账号" prop="account" min-width="120"/>
+        <el-table-column label="身份" prop="realName" min-width="230">
+          <template slot-scope="scope">
+            <el-tag size="small" type="info" v-for="(item, index) in scope.row.roleNames.split(',')" class="mr5">{{ item }}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="最后登录时间" prop="lastTime" min-width="180">
+          <template slot-scope="scope">
+            <span>{{ scope.row.lastTime | filterEmpty }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="最后登录IP" prop="lastIp" min-width="150">
+          <template slot-scope="scope">
+            <span>{{ scope.row.lastIp | filterEmpty }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="状态" prop="status" min-width="100">
+          <template slot-scope="scope">
+            <span>{{ scope.row.status | filterShowOrHide }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="删除标记" prop="status" min-width="100">
+          <template slot-scope="scope">
+            <span>{{ scope.row.isDel | filterYesOrNo }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" min-width="130"  fixed="right">
+          <template slot-scope="scope">
+            <template v-if="scope.row.isDel">
+              <span>-</span>
+            </template>
+            <template v-else>
+              <el-button type="text" size="mini" @click="handlerOpenEdit(1,scope.row)">编辑</el-button>
+              <el-button type="text" size="mini" @click="handlerOpenDel(scope.row)">删除</el-button>
+            </template>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        :current-page="listPram.page"
+        :page-sizes="constants.page.limit"
+        :layout="constants.page.layout"
+        :total="listData.total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </el-card>
+    <el-dialog
+      :visible.sync="editDialogConfig.visible"
+      :title="editDialogConfig.isCreate === 0? '创建身份':'编辑身份'"
+      destroy-on-close
+      :close-on-click-modal="false"
+    >
+      <edit
+        v-if="editDialogConfig.visible"
+        :is-create="editDialogConfig.isCreate"
+        :edit-data="editDialogConfig.editData"
+        @hideEditDialog="hideEditDialog"
+      />
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import * as systemAdminApi from '@/api/systemadmin.js'
+import * as roleApi from '@/api/role.js'
+import * as constants from '@/utils/constants.js'
+import edit from './edit'
+export default {
+  // name: "index"
+  components: { edit },
+  data() {
+    return {
+      constants,
+      listData: { list: [] },
+      listPram: {
+        account: null,
+        addTime: null,
+        lastIp: null,
+        lastTime: null,
+        level: null,
+        loginCount: null,
+        realName: null,
+        roles: null,
+        status: null,
+        page: 1,
+        limit: constants.page.limit[0]
+      },
+      roleList: [],
+      menuList: [],
+      editDialogConfig: {
+        visible: false,
+        isCreate: 0, // 0=创建，1=编辑
+        editData: {}
+      }
+    }
+  },
+  mounted() {
+    this.handleGetAdminList()
+    this.handleGetRoleList()
+  },
+  methods: {
+    handleSearch() {
+      this.listPram.page = 1
+      this.handleGetAdminList()
+    },
+    handleSizeChange(val) {
+      this.listPram.limit = val
+      this.handleGetAdminList()
+      this.handleGetRoleList(this.listPram)
+    },
+    handleCurrentChange(val) {
+      this.listPram.page = val
+      this.handleGetAdminList()
+      this.handleGetRoleList(this.listPram)
+    },
+    handleGetRoleList() {
+      const _pram = {
+        page: 1,
+        limit: constants.page.limit[4]
+      }
+      roleApi.getRoleList(_pram).then(data => {
+        this.roleList = data
+      })
+    },
+    handlerOpenDel(rowData) {
+      this.$confirm('确认删除当前数据').then(() => {
+        const _pram = { id: rowData.id}
+        systemAdminApi.adminDel(_pram).then(data => {
+          this.$message.success('删除数据成功')
+          this.handleGetAdminList()
+        })
+      })
+    },
+    handleGetAdminList() {
+      systemAdminApi.adminList( this.listPram ).then(data => {
+        this.listData = data
+        // this.handlerGetMenuList()
+      })
+    },
+    handlerOpenEdit(isCreate, editDate) {
+      this.editDialogConfig.editData = editDate
+      this.editDialogConfig.isCreate = isCreate
+      this.editDialogConfig.visible = true
+    },
+    handlerGetMenuList() { // 获取菜单全部数据后做menu翻译使用
+      systemAdminApi.listCategroy({ page: 1, limit: 999, type: 5 }).then(data => {
+        this.menuList = data.list
+        this.listData.list.forEach(item => {
+          const _muneText = []
+          const menuids = item.rules.split(',')
+          menuids.map(muid => {
+            this.menuList.filter(menu => {
+              if (menu.id == muid) {
+                _muneText.push(menu.name)
+              }
+            })
+          })
+          item.rulesView = _muneText.join(',')
+          this.$set(item, 'rulesViews', item.rulesView)
+        })
+      })
+    },
+    hideEditDialog() {
+      this.editDialogConfig.visible = false
+      this.handleGetAdminList()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
