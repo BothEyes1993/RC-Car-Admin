0eb7396b13b826208dfc06d50712691e10815181
BothEyes <1993046671@qq.com>
2020-10-17 17:24:18 +0800

    t

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ module.exports = {
   collectCoverageFrom: ['src/utils/**/*.{js,vue}', '!src/utils/auth.js', '!src/utils/request.js', 'src/components/**/*.{js,vue}'],
   coverageDirectory: '<rootDir>/tests/unit/coverage',
   // 'collectCoverage': true,
-  'coverageReporters': [
+  coverageReporters: [
     'lcov',
     'text-summary'
   ],
